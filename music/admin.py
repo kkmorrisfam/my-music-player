10c016ec8717d69c8c460de52af3e4f95ba6a2ba
Kerri Morris <kkmorrisfam@hotmail.com>
2025-09-20 22:22:42 -0700

    add inline interface in admin for join tables

--- music/admin.py
+++ music/admin.py
@@ -1,16 +1,42 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import (
     Artist, Album, Track, Category, TrackArtist,
     AlbumArtist, AlbumTrack, Favorite,
     PlayHistory, Playlist, PlaylistTrack
 )
 
-# Register everything so it appears in /admin
+# The admin interface has the ability to edit models on the same page 
+# as a parent model. These are called inlines.
+# add join tables on parent forms
+class AlbumTrackInline(admin.TabularInline):
+    model = AlbumTrack
+    extra = 1   # show 1 empty row by default
+
+class TrackArtistInline(admin.TabularInline):
+    model = TrackArtist
+    extra = 1
+
+class AlbumArtistInline(admin.TabularInline):
+    model = AlbumArtist
+    extra = 1
+
+
+# Register your models here.
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [AlbumArtistInline, AlbumTrackInline]
+
+@admin.register(Track)
+class TrackAdmin(admin.ModelAdmin):
+    list_display = ("title",)
+    inlines = [TrackArtistInline]
+    filter_horizontal = ("categories",) 
+
 admin.site.register(Artist)
-admin.site.register(Album)
-admin.site.register(Track)
+#admin.site.register(Album)
+#admin.site.register(Track)
 admin.site.register(Category)
 
 
@@ -19,3 +45,8 @@ admin.site.register(Favorite)
 admin.site.register(PlayHistory)
 admin.site.register(Playlist)
 admin.site.register(PlaylistTrack)
+
+# join tables
+admin.site.register(TrackArtist)
+admin.site.register(AlbumArtist)
+admin.site.register(AlbumTrack)
