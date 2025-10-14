5a33ae9bd41209f563a64a2ba6af5b43a83027bc
Kerri Morris <kkmorrisfam@hotmail.com>
2025-10-14 16:53:15 -0700

    cleanup forms

--- music/forms.py
+++ music/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 # from django.forms.widgets import HiddenInput
 from . import models
-# class AddPlaylistForm(forms.Form):
-#     name = forms.CharField(label="Playlist name", max_length=200)
-#     created_at = forms.DateTimeField(widget=HiddenInput(), required=False)
 
 class AddPlaylistForm(forms.ModelForm):
     class Meta:
